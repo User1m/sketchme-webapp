fb284653af57dea90ab48c1329ae890755ce59dd
Claudius Mbemba <mbembac@gmail.com>
2018-04-01 18:03:57 -0700

    updated docker structure

---
+++ docker/scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "missing image prod tag. use 'latest' if you want 'latest' tag"
+	exit 1
+fi
+
+docker build -t sketchme-webapp:prod-$1 \
+-f /Users/claudius/Documents/workspace/_ML/sketchme/sketchme-docker/sketchme-webapp/docker/Dockerfile \
+/Users/claudius/Documents/workspace/_ML/sketchme/sketchme-docker/sketchme-webapp/.
